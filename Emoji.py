0c87e60d39ed64bfb8a33dde15749c4252d201ea
Nirmith-M-R <136159356+Nirmith-M-R@users.noreply.github.com>
2023-07-08 09:05:34 +0530

    Update Emoji.py

--- Emoji.py
+++ Emoji.py
@@ -6,6 +6,6 @@ st.markdown("Emoji's Multiplier :smile:")
 emojis=[":smile:",":100:",":rice:", ":cookie:", ":chocolate_bar:", ":grinning:",":grin:",":joy:",":smiley:",":sweat_smile:"]
 emoji=st.selectbox("Select emoji", emojis)
 
-count=st.number_input("Enter count",value=5,min_value=1,max_value=200,step=1)
+count=st.number_input("Enter count",value=0,min_value=1,max_value=200,step=1)
 
 st.write(emoji*int(count))
